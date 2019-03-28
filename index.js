601d30b7cb78123e8b48efbeff54a8b13ad01cee
Nicoco220983 <nicolas.carrez@gmail.com>
2019-03-29 00:23:55 +0100

    Use refactored sheet

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const msaPage = module.exports = new Msa.Module()
 // user
 const { mdw:userMdw } = Msa.require("user")
 // sheet
-const msaSheet = Msa.require("sheet")
-
+const MsaSheet = Msa.require("sheet/module")
+const msaSheet = new MsaSheet("page")
+/*
 // register page sheets
 msaSheet.registerType("page", {
 	perms: {
@@ -17,17 +18,14 @@ msaSheet.registerType("page", {
 		}
 	}
 })
-
-const renderSheetAsHtml = msaSheet.renderSheetAsHtml
-msaPage.app.get("/:key", userMdw, (req, res, next) => {
-	const pageKey = req.params.key
-	msaSheet.getSheet("page", pageKey,
-		{ user:req.session.user, ifNotExist:"create", insertInDb:false },
-		(err, sheet) => {
-			// page not found => blank page
-			if(err===404) res.sendPage("")
-			else if(err) return next(err)
-			else res.sendPage(renderSheetAsHtml(sheet))
-			next()
-		})
+*/
+msaPage.app.get("/:key", userMdw, async (req, res, next) => {
+	try {
+		const { key } = req.params
+		const sheet = await msaSheet.getSheet(req, key)
+		if(sheet === null) return next(404)
+		res.sendPage(msaSheet.renderSheetAsHtml(sheet, "/page/_sheet", key))
+	} catch(err) { next(err) }
 })
+
+msaPage.app.use("/_sheet", msaSheet.app)
